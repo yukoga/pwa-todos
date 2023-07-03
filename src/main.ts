2720571c19dbb4446cc61065a848a0a1f02fd601
yukoga <yukoga@gmail.com>
2023-07-03 19:44:55 +0900

    WIP - cleanup templates for further development.

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+const content_container = document.querySelector<HTMLDivElement>('div.container')!
+const innerHTML = `
+  <div class="row d-flex justify-content-center align-items-center hogefuga">
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
@@ -20,5 +23,13 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </p>
   </div>
 `
+const inner_content = ((html) => {
+  const div = document.createElement('div')
+  div.innerHTML = html.trim()
+  return div.firstChild
+})(innerHTML)
+
+content_container.insertAdjacentElement('afterbegin', inner_content)
+/**/
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
